d7939ebdddde3ec8729a6daaca53ced0fc04cc8a
Sn2910 <sneha.shaw29@gmail.com>
2022-08-13 17:56:07 +0200

    Fetching Data from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import TravelBlog from './Components/TravelBlog';
 
 function App() {
   return (
     <div className="App">
-     Hello There
+   <TravelBlog />
     </div>
   );
 }
