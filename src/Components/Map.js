486a8c5f662a385e2f6ae2ec829fc062d0735b93
Sn2910 <sneha.shaw29@gmail.com>
2022-08-27 10:50:53 +0200

    deployed

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,13 +1,16 @@
 import React from 'react'
-
+const handleOnClick = (e) => {
+  e.preventDefault();
+  console.log("You have clicked in the specified area")
+}
 function Map() {
   return (
     <div class="large_map">
 
     <img src={require("../Images/europe.jpg")} usemap="#map-europe" width="720" height="489"/>
-
+    
 <map name="map-europe">
-<area alt="Germany" title="Germany" href="/Europe/Germany/" coords="465,472,548,559,502,680,414,669,393,570" ></area>
+<area alt="Germany" title="Germany" href="/Europe/Germany/" coords="465,472,548,559,502,680,414,669,393,570" onClick={handleOnClick}></area>
 </map>
 </div>
   )
