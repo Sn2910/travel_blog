f603a78cdd20a140b29779cffbec89495b0a21e7
Sn2910 <sneha.shaw29@gmail.com>
2022-08-27 12:03:55 +0200

    Update Map.js

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,18 +1,17 @@
 import React from 'react'
-const handleOnClick = (e) => {
-  e.preventDefault();
+const handleOnClick = () => {
   console.log("You have clicked in the specified area")
 }
 function Map() {
   return (
-    <div class="large_map">
+    <div className="large_map">
 
-    <img src={require("../Images/europe.jpg")} usemap="#map-europe" width="720" height="489"/>
-    
-<map name="map-europe">
-<area alt="Germany" title="Germany" href="/Europe/Germany/" coords="465,472,548,559,502,680,414,669,393,570" onClick={handleOnClick}></area>
-</map>
-</div>
+      <img src={require("../Images/europe.jpg")} useMap="#map-europe" width="720" height="489" />
+
+      <map name="map-europe">
+        <area shape ="poly" alt="Germany" title="Germany"  coords="465,472,548,559,502,680,414,669,393,570" onClick={()=>handleOnClick} ></area>
+      </map>
+    </div>
   )
 }
 
