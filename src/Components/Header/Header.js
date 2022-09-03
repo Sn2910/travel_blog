24aee37350915daa159d7edd68f8e687225f6887
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2022-09-03 10:04:50 +0200

    Styling started

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,29 +1,42 @@
-import React from 'react'
-import './Header.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div>
-        <header>
-            <h1>Travel<span>Blog</span></h1>
-            <nav className='navLinks'>
-                <Link to="#">Home</Link>
-                <Link to="#">About</Link>
-                <Link to="#">Contact</Link>
-                <Link to="#">Destinations
-                {/* <div>
+      <header>
+        <h1>
+          Travel<span>Blog</span>
+        </h1>
+        <nav className="navLinks">
+          <Link to="/">Home</Link>
+          <Link to="#">About</Link>
+          <Link to="#">Contact</Link>
+          <Link to="#">
+            Destinations
+            {/* <div>
                     <ul>
-                        <li><Link to="#">Austria</Link></li>
+                        <li><Link to="{`/travel-blog/${destinations[0].sys.id}`}">Austria</Link></li>
                         <li><Link to="#">Romania</Link></li>
                         <li><Link to="#">Netherlands</Link></li>
                         <li><Link to="#">France</Link></li>
                         <li><Link to="#">Germany</Link></li>
                     </ul>
                 </div> */}
-                </Link>
-            </nav>
-        </header>
+          </Link>
+        </nav>
+      </header>
+      <div>
+        <h2>Tour Around The World</h2>
+        <p>
+          We're passionate about helping travelers to experience all the best in
+          <br />
+          Ghana. Find everything right here, from god culture, lovely people,
+          <br />
+          and all wonderful tourist attractions
+        </p>
+      </div>
     </div>
-  )
+  );
 }
