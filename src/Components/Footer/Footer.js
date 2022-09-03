309564e495b60054cdeabe30953de777621b4da4
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2022-09-03 15:46:40 +0200

    still working on styling

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div>
+      <Outlet />
+      <div className="footer">
+        <p>
+          Copyright &copy; 2022 Travel<span>Blog</span>
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/Components/TravelInfo/TravelInfo.js
+++ src/Components/TravelInfo/TravelInfo.js
@@ -1,54 +1,78 @@
-import React from 'react'
+import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import  {getAsset} from '../../controllers/content'
-import Header from '../Header/Header'
-import '../TravelInfo/TravelInfo.css'
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { getAsset } from "../../controllers/content";
+import Header from "../Header/Header";
+import "../TravelInfo/TravelInfo.css";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 function TravelInfo() {
-    const [getInfo, setGetInfo] = useState(false);
-    const [assets, setAssets] = useState(false)
-    const apiHost = "https://cdn.contentful.com";
-    const {id} = useParams();
- 
-    const travelInfo = async () => {
-        const response = await fetch(`${apiHost}/spaces/${process.env.REACT_APP_SPACE_ID}/environments/${process.env.REACT_APP_ENVIRONMENT}/entries/${id}?access_token=${process.env.REACT_APP_ACCESS_TOKEN}`)
-        const result = await response.json();
-        
-        setGetInfo(result);
-        console.log("Travel")
-        console.log(result);
-      };
-      useEffect(() => {
-        travelInfo();
-        getAsset().then((assets)=>{setAssets(assets)})
-      }, [id]);
-      if(!getInfo || !assets){
-        return <div>Loading...</div>
-      }
-      function getAssetUrl(assetId){
-        const found = assets.items.find((e)=>e.sys.id === assetId)
-     /* console.log(assetId) */
-      console.log(found)
-      if(!found){
-      return ""
-    
-      }
-      return "https:" + found.fields.file.url;
-      }
-      const backgroundUrl = getAssetUrl(getInfo.fields.bgImg.sys.id)
+  const [getInfo, setGetInfo] = useState(false);
+  const [assets, setAssets] = useState(false);
+  const apiHost = "https://cdn.contentful.com";
+  const { id } = useParams();
+
+  const travelInfo = async () => {
+    const response = await fetch(
+      `${apiHost}/spaces/${process.env.REACT_APP_SPACE_ID}/environments/${process.env.REACT_APP_ENVIRONMENT}/entries/${id}?access_token=${process.env.REACT_APP_ACCESS_TOKEN}`
+    );
+    const result = await response.json();
+
+    setGetInfo(result);
+    console.log("Travel");
+    console.log(result);
+  };
+  useEffect(() => {
+    travelInfo();
+    getAsset().then((assets) => {
+      setAssets(assets);
+    });
+  }, [id]);
+  if (!getInfo || !assets) {
+    return <div>Loading...</div>;
+  }
+  function getAssetUrl(assetId) {
+    const found = assets.items.find((e) => e.sys.id === assetId);
+    /* console.log(assetId) */
+    console.log(found);
+    if (!found) {
+      return "";
+    }
+    return "https:" + found.fields.file.url;
+  }
+  const backgroundUrl = getAssetUrl(getInfo.fields.bgImg.sys.id);
   return (
-    <div>
-    
-        <h1>{getInfo.fields.city} in {getInfo.fields.country}</h1>
-        <img className='city_bg' src={backgroundUrl} style={{width:"inherit"}} />
-        <div>
-                {documentToReactComponents(getInfo.fields.cityInfo)  }
-            </div>
-      
+    <div className="cityInfoContent">
+      <div
+        className="infoBgImage"
+        style={{
+          height: "550px",
+          backgroundImage: `url(${backgroundUrl})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          borderRadius: "0px",
+          objectFit: "cover",
+        }}
+      >
+        <h1
+          style={{
+            textAlign: "center",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            height: "inherit",
+            width: "inherit",
+            color: "#fff",
+            textShadow: "2px 2px 2px orange",
+            fontSize: "3em",
+          }}
+        >
+          {getInfo.fields.city} in {getInfo.fields.country}
+        </h1>
+      </div>
+      <div>{documentToReactComponents(getInfo.fields.cityInfo)}</div>
     </div>
-  )
+  );
 }
 
-export default TravelInfo
+export default TravelInfo;
