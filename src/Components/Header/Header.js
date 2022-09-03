9ee7b4afd39d74edcddaf0eb59e39b74ee17b29d
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2022-09-03 16:43:36 +0200

    issues fixed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,42 +1,46 @@
-import React from 'react'
-import './Header.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
 
-
- function Header({destinations}) {
-    console.log(destinations)
+function Header({ destinations }) {
+  console.log(destinations);
   return (
     <div>
-        <header>
-            <h1>Travel<span>Blog</span></h1>
-            <nav className='navLinks'>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to= "">Destinations
-                 <div>
-                
-                    
-                   <ul>
-                    {destinations.map((destination,index)=>{
-                        return(
-                            <li><Link to={`/travel-blog/${destination.sys.id}`}  key={index}>{destination.fields.country}</Link></li>
-                        )
-                    }
-                    
-                    )}
-                        {/* li><Link to="/travel-blog/6Sr73RpHDWl8Bjc6CTXqhK">Austria</Link></li>
+      <header>
+        <h1>
+          Travel<span>Blog</span>
+        </h1>
+        <nav className="navLinks">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="" className="dest">
+            Destinations
+            <div>
+              <ul>
+                {destinations.map((destination, index) => {
+                  return (
+                    <li>
+                      <Link
+                        to={`/travel-blog/${destination.sys.id}`}
+                        key={index}
+                      >
+                        {destination.fields.country}
+                      </Link>
+                    </li>
+                  );
+                })}
+                {/* li><Link to="/travel-blog/6Sr73RpHDWl8Bjc6CTXqhK">Austria</Link></li>
                         <li><Link to="#">Romania</Link></li>
                         <li><Link to="#">Netherlands</Link></li>
                         <li><Link to="#">France</Link></li>
                         <li><Link to="#">Germany</Link></li> */}
-                    </ul> 
-                </div> 
-                </Link>
-               
-            </nav>
-        </header>
+              </ul>
+            </div>
+          </Link>
+        </nav>
+      </header>
     </div>
-  )
+  );
 }
-export default Header
+export default Header;
