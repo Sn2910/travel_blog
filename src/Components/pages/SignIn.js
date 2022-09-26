1dc1933ebd11d0de8546a34aeb9b25141d067890
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2022-09-27 00:53:54 +0200

    SignUp and SignIn styling completed

--- src/Components/pages/SignIn.js
+++ src/Components/pages/SignIn.js
@@ -1,5 +1,30 @@
 import React from "react";
+import "./UserSigning.css";
+import { Container } from "@mui/material";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div className="singInForm-Cont">
+      <Container maxWidth="sm" className="signInWrap">
+        <p>Kindly fill in all fields to sign in!</p>
+        <form className="singInForm">
+          <div>
+            <h4>Username:</h4>
+            <input type="text" />
+          </div>
+          <div>
+            <h4>Email:</h4>
+            <input type="email" />
+          </div>
+          <div>
+            <h4>Password:</h4>
+            <input type="password" />
+          </div>
+          <div className="signIn-Btn">
+            <button>Sign-In</button>
+          </div>
+        </form>
+      </Container>
+    </div>
+  );
 }
--- src/Components/pages/SignUp.js
+++ src/Components/pages/SignUp.js
@@ -1,5 +1,48 @@
 import React from "react";
+import "./UserSigning.css";
+import { Container } from "@mui/material";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="singUpForm-Cont">
+      <Container maxWidth="sm" className="signUpWrap">
+        <p>Kindly fill in all fields to create your new account!</p>
+        <form className="singUpForm">
+          <div className="userFullName">
+            <div>
+              <h4>First Name:</h4>
+              <input type="text" />
+            </div>
+            <div>
+              <h4>Last Name:</h4>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="userName-Email">
+            <div>
+              <h4>Username:</h4>
+              <input type="text" />
+            </div>
+            <div>
+              <h4>Email:</h4>
+              <input type="email" />
+            </div>
+          </div>
+          <div className="password">
+            <div>
+              <h4>Password:</h4>
+              <input type="password" />
+            </div>
+            <div>
+              <h4>Confirm Password:</h4>
+              <input type="password" />
+            </div>
+          </div>
+          <div className="createAcc">
+            <button>Create New Account</button>
+          </div>
+        </form>
+      </Container>
+    </div>
+  );
 }
